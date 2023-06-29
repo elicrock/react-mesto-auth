9add9bd7bbf385105160effb36e7645a7925ab05
Paul <paul.ivanov1990@gmail.com>
2023-06-29 15:59:01 +0300

    Sprint 12 - after review add mobile menu

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,34 +1,55 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 
 function Header({ isLoggedIn, link, linkText, email, signOut }) {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [isMobile, setIsMobile] = useState(false);
 
   function handleMenuClick() {
     setIsMenuOpen(!isMenuOpen);
   }
 
+  useEffect(() => {
+    const updateWindowSize = () => {
+      setIsMobile(window.innerWidth <= 576);
+    };
+
+    window.addEventListener('resize', updateWindowSize);
+    updateWindowSize();
+
+    return () => {
+      window.removeEventListener('resize', updateWindowSize);
+    };
+  }, []);
+
   return (
     <header className="header">
       <Link to="/react-mesto-auth" className="header__logo" />
-      <nav className={`header__links ${isMenuOpen ? 'header__links_mobile-open' : ''}`}>
-        {isLoggedIn && <p className="header__email">{email}</p>}
-        <Link to={link} onClick={signOut} className={`header__link ${isLoggedIn && 'header__link_islogin'}`}>
-          {linkText}
-        </Link>
-      </nav>
-      <button className="header__burger" onClick={handleMenuClick}>
-        <span className={`header__burger-line ${isMenuOpen ? 'header__burger-line_close' : ''}`}></span>
-      </button>
+      {
+        isMobile && isLoggedIn && isMenuOpen && (
+          <nav className="header__links header__links_mobile-open">
+            <p className="header__email">{email}</p>
+            <Link to={link} onClick={signOut && handleMenuClick} className="header__link header__link_islogin">
+              {linkText}
+            </Link>
+          </nav>
+        )
+      }
+      {
+        isMobile && isLoggedIn ? (
+          <button className="header__burger" onClick={handleMenuClick}>
+            <span className={`header__burger-line ${isMenuOpen ? 'header__burger-line_close' : ''}`}></span>
+          </button>
+        ) : (
+          <nav className="header__links">
+            {isLoggedIn && <p className="header__email">{email}</p>}
+            <Link to={link} onClick={signOut} className={`header__link ${isLoggedIn ? 'header__link_islogin' : ''}`}>
+              {linkText}
+            </Link>
+          </nav>
+        )
+      }
     </header>
-
-    // <header className="header">
-    //   <Link to="/react-mesto-auth" className="header__logo" />
-    //   <nav className="header__links">
-    //     {isLoggedIn && <p className="header__email">{email}</p>}
-    //     <Link to={link} onClick={signOut} className={`header__link ${isLoggedIn && 'header__link_islogin'}`}>{linkText}</Link>
-    //   </nav>
-    // </header>
   );
 }
 
